21627e50e78895c6f5b7e649e986ccfa700c4c1c
EmMDC <143551026+EmMDC@users.noreply.github.com>
2023-10-05 22:48:00 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+// import all necessary dependencies
+import { useEffect, useState } from 'react'
+import './styles.css'
+import Title from './Title.js'
+import Entry from './Entry.js'
+import Info from './info.js'
+import { getCharacter } from './api.js'
+import Dancing from './Dancing.gif'
+
+//main app content
+export default function App() {
+  //varaibles for name, data, loading, and error
+  const [name, setName] = useState('')
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  //effect to get character data
+  useEffect(() => {
+    if (!name) {
+      return
+    }
+
+    async function fetchData() {
+      try {
+        const character = await getCharacter(name)
+        setData(character)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+      }
+    }
+    
+    //set the states before fetching the data
+    setLoading(true)
+    setData(null)
+    setError(null)
+    fetchData()
+  }, [name])
+
+  //the 'html' of the app
+  return (
+    <div className="App">
+      <Title text="Star Wars Characters" />
+      <Entry action={setName}/>
+
+      {loading && <div className="loader"></div>}
+
+      {error ? <span>Something went wrong.</span> : <Info name={name} data={data} />}
+      <img src={Dancing} alt="Dancing Darth Vader" className="center" />
+    </div>
+  )
+}
