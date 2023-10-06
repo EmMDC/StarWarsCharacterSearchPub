99e4dd9c0a90b8c1908836ab1eb93b913ccc9461
EmMDC <143551026+EmMDC@users.noreply.github.com>
2023-10-05 22:04:41 -0700

    help user understand what to do

--- src/Entry.js
+++ src/Entry.js
@@ -1,17 +1,18 @@
-import { useState } from "react";
-
+import { useState } from 'react'
+//this is to handle the users input
 export default function Entry({ action }) {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('')
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    action(name);
-    setName("");
-  };
-
+  //handlesform submission
+  const handleSubmit = e => {
+    e.preventDefault()
+    action(name)
+    setName('')
+  }
+  //the 'html' for the submission box
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder='Enter Character'/>
     </form>
-  );
+  )
 }
